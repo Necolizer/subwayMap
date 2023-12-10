80a4ce5648dbb8b37cf14d8fe0860298f19365f1
Necolizer <58028682+Necolizer@users.noreply.github.com>
2023-12-10 14:49:25 +0800

    v1

--- static/js/index.js
+++ static/js/index.js
@@ -39,19 +39,29 @@ function drawCircle(cx, cy, radius, fill, stroke, stroke_width, opacity, fill_op
     return circle;
 }
 
-const jsonFilePath = './static/json/广州.json';
+const jsonFilePath = './static/json/成都.json';
 const visualization = document.getElementById('visualization');
 const raw_width = 1980;
 const raw_height = 1280;
 const city_boundary = 250;
 const draw_offset = 25;
-const city_circle_r = 145;
+const city_circle_r = 142;
+
+function distort(x, k) {
+    return Math.tanh(k * (2 * x - 1)) / 2 + 0.5;
+}
 
 function splitPString(s) {
     const numberStrings = s.split(' ');
-    const number1 = draw_offset + Math.max(0, parseInt(numberStrings[0], 10) / raw_width * city_boundary);
-    const number2 = draw_offset + Math.max(0, parseInt(numberStrings[1], 10) / raw_height * city_boundary);
+    const number1 = draw_offset + Math.max(0, parseInt(numberStrings[0], 10) / raw_width) * city_boundary;
+    const number2 = draw_offset + Math.max(0, parseInt(numberStrings[1], 10) / raw_height) * city_boundary;
     return [number1, number2];
+}
+
+function calculateRadius(x1, y1, x2, y2) {
+    const distance = Math.sqrt(Math.pow(x2 - x1, 2) + Math.pow(y2 - y1, 2));
+    const radius = distance / 2;
+    return radius;
 }
 
 const svg = document.createElementNS("http://www.w3.org/2000/svg", "svg");
@@ -64,7 +74,7 @@ function parseData(jsonData){
     const city = jsonData.s;
     const city_id = jsonData.i;
 
-    city_circle = drawCircle(150, 150, city_circle_r, '#E9D4C7', '#261E25', '9px', '0.9', '0.3');
+    city_circle = drawCircle(150, 150, city_circle_r, '#E9D4C7', '#261E25', '15px', '0.9', '0.6');
     svg.appendChild(city_circle);
 
     let max_st_num = 0;
@@ -92,14 +102,20 @@ function parseData(jsonData){
             }
         });
         if (is_loop == '1'){
-            
+            const first_station = subwayline.st[0];
+            const [x1, y1] = splitPString(first_station.p);
+            const mid_station = subwayline.st[Math.floor(station_num/2)];
+            const [x2, y2] = splitPString(mid_station.p);
+            thickness = 0.25 + (station_num-min_st_num)/(max_st_num-min_st_num) * 4.25;
+            circle = drawCircle((x1+x2)/2, (y1+y2)/2, calculateRadius(x1, y1, x2, y2), '#' + color, '#' + color, thickness.toFixed(2) + 'px', '0.7', '0.5');
+            svg.appendChild(circle);
         }else
         {
             const first_station = subwayline.st[0];
             const [x1, y1] = splitPString(first_station.p);
             const last_station = subwayline.st[station_num - 1];
             const [x2, y2] = splitPString(last_station.p);
-            thickness = 0.25 + (station_num-min_st_num)/(max_st_num-min_st_num) * 2.75;
+            thickness = 0.25 + (station_num-min_st_num)/(max_st_num-min_st_num) * 4.25;
             line = drawLine(x1, y1, x2, y2, '#' + color, thickness.toFixed(2) + 'px', '0.7');
             svg.appendChild(line);
         }
